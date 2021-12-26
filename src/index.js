bd7a4fd8a50492704e7f39cec198c1566fb0dff5
Igor Bari <mucsi96@gmail.com>
2021-12-26 20:58:47 +0100

    replaceInFile

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { readdir, readFile } = require("fs/promises");
+const { readdir, readFile, writeFile } = require("fs/promises");
 const { resolve } = require("path");
 
 async function listFilesInFolder(path, options = {}) {
@@ -31,20 +31,62 @@ async function listVariablesInFile(path) {
   return [...source.matchAll(/@(\w+);/g)].map((match) => match[1]);
 }
 
-async function listLessVariables(path) {
-  const lessFiles = await listFilesInFolder(path, {
-    ignore: [".git"],
-    patterns: [/.less$/],
-  });
+async function replaceInFile(path, items) {
+  const source = await readFile(path, "utf8");
+  console.log(items);
+  const replacedSource = items.reduce(
+    (acc, item) => acc.replaceAll(item[0], item[1]),
+    source
+  );
+  await writeFile(`${path}.new`, replacedSource, "utf8");
+}
+
+async function listLessVariables(path, options) {
+  const lessFiles = await listFilesInFolder(path, options);
 
   const lessVariables = await Promise.all(lessFiles.map(listVariablesInFile));
 
   return [...new Set(lessVariables.flatMap((item) => item))];
 }
 
+async function mapLessVariables(path, variableMap, options) {
+  const lessFiles = await listFilesInFolder(path, options);
+  const lessVariables = await listLessVariables(path, options);
+  const unmappedVariable = lessVariables.find(
+    (variable) => !variableMap[variable]
+  );
+
+  if (unmappedVariable) {
+    throw new Error(`Variable "@${unmappedVariable}" missing in map.`);
+  }
+
+  const itemToReplace = Object.entries(variableMap).map(([from, to]) => [
+    new RegExp(`@${from};`, "g"),
+    `var(${to});`,
+  ]);
+  await Promise.all(
+    lessFiles.map((path) => replaceInFile(path, itemToReplace))
+  );
+}
+
 async function main() {
-  const lessVariables = await listLessVariables(resolve(__dirname, ".."));
+  const path = resolve(__dirname, "..");
+  const options = {
+    ignore: [".git"],
+    patterns: [/.less$/],
+  };
+  const lessVariables = await listLessVariables(path, options);
   console.log(lessVariables);
+
+  await mapLessVariables(
+    path,
+    {
+      green: "--color-green",
+      blue: "--color-blue",
+      text: "--color-text"
+    },
+    options
+  );
 }
 
 main().catch(console.error);
