9cb5bcd558619b81c068858d32c55fa9a1253bfd
Igor Bari <mucsi96@gmail.com>
2021-12-26 16:10:08 +0100

    listLessVariables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { readdir } = require("fs/promises");
+const { readdir, readFile } = require("fs/promises");
 const { resolve } = require("path");
 
 async function listFilesInFolder(path, options = {}) {
@@ -7,15 +7,15 @@ async function listFilesInFolder(path, options = {}) {
 
   const files = await Promise.all(
     dirEntries.map((dirEntry) => {
+      const filePath = resolve(path, dirEntry.name);
+
       if (ignore.includes(dirEntry.name)) {
         return undefined;
       }
 
       if (dirEntry.isDirectory()) {
-        return listFilesInFolder(resolve(path, dirEntry.name), options);
+        return listFilesInFolder(filePath, options);
       }
-
-      const filePath = resolve(path, dirEntry.name);
 
       if (patterns.some((pattern) => pattern.test(filePath))) {
         return filePath;
@@ -26,9 +26,25 @@ async function listFilesInFolder(path, options = {}) {
   return files.flatMap((file) => file).filter(Boolean);
 }
 
-listFilesInFolder(resolve(__dirname, ".."), {
-  ignore: [".git"],
-  patterns: [/.less$/],
-})
-  .then(console.log)
-  .catch(console.error);
+async function listVariablesInFile(path) {
+  const source = await readFile(path, "utf8");
+  return [...source.matchAll(/@(\w+);/g)].map((match) => match[1]);
+}
+
+async function listLessVariables(path) {
+  const lessFiles = await listFilesInFolder(path, {
+    ignore: [".git"],
+    patterns: [/.less$/],
+  });
+
+  const lessVariables = await Promise.all(lessFiles.map(listVariablesInFile));
+
+  return [...new Set(lessVariables.flatMap((item) => item))];
+}
+
+async function main() {
+  const lessVariables = await listLessVariables(resolve(__dirname, ".."));
+  console.log(lessVariables);
+}
+
+main().catch(console.error);
